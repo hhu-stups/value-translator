25db924ef2a1b5547aacf4693defc996525a1cad
dgelessus <dgelessus@users.noreply.github.com>
2024-05-14 14:31:56 +0200

    Add Translator.translate overloads that take a parsed AST
    So that external code doesn't need to use TranslatingVisitor directly.

--- src/main/java/de/hhu/stups/prob/translator/Translator.java
+++ src/main/java/de/hhu/stups/prob/translator/Translator.java
@@ -2,7 +2,10 @@ package de.hhu.stups.prob.translator;
 
 import de.be4.classicalb.core.parser.BParser;
 import de.be4.classicalb.core.parser.exceptions.BCompoundException;
-import de.be4.classicalb.core.parser.node.Node;
+import de.be4.classicalb.core.parser.node.AExpressionParseUnit;
+import de.be4.classicalb.core.parser.node.PExpression;
+import de.be4.classicalb.core.parser.node.PParseUnit;
+import de.be4.classicalb.core.parser.node.Start;
 import de.hhu.stups.prob.translator.exceptions.TranslationException;
 
 public final class Translator {
@@ -10,19 +13,41 @@ public final class Translator {
         // only static access
     }
 
-    public static <T extends BValue> T translate(final String expression)
+    public static <T extends BValue> T translate(final PExpression ast)
             throws TranslationException {
-
         try {
-            final Node ast = BParser.parse("#EXPRESSION" + expression);
             final TranslatingVisitor<T> visitor = new TranslatingVisitor<>();
             ast.apply(visitor);
             return visitor.getResult();
-        } catch (final BCompoundException
-                               | TranslatingVisitor.UnexpectedTypeException
+        } catch (final TranslatingVisitor.UnexpectedTypeException
                                | TranslatingVisitor.IllegalStateException
                          exception) {
             throw new TranslationException(exception);
         }
+    }
+
+    public static <T extends BValue> T translate(final Start ast)
+            throws TranslationException {
+        final PParseUnit parseUnit = ast.getPParseUnit();
+        if (!(parseUnit instanceof AExpressionParseUnit)) {
+            throw new TranslationException(
+                new TranslatingVisitor.IllegalStateException(
+                    "Only expressions can be translated, but received a "
+                    + parseUnit.getClass()
+                )
+            );
+        }
+        return translate(((AExpressionParseUnit) parseUnit).getExpression());
+    }
+
+    public static <T extends BValue> T translate(final String expression)
+            throws TranslationException {
+        final Start ast;
+        try {
+            ast = BParser.parse("#EXPRESSION" + expression);
+        } catch (final BCompoundException exception) {
+            throw new TranslationException(exception);
+        }
+        return translate(ast);
     }
 }
